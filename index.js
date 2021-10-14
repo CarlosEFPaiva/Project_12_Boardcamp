c343e4a3c4bc34a59c5e4df624341b298af044f2
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2021-10-14 11:57:29 -0300

    implemented: created GET function for route '/categories', which communicate with the database and returns an array with all listed categories

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from "express";
 import cors from "cors";
+import pg from "pg";
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+const { Pool } = pg;
+
+const connection = new Pool({
+    user: 'bootcamp_role',
+    password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
+    host: 'localhost',
+    port: 5432,
+    database: 'boardcamp'
+});
+
+server.get("/categories", (req, resp) => {
+    const promise = connection.query('SELECT * FROM categories')
+    .then( result => {
+        resp.send(result.rows);
+    } )
+})
+
 
 server.listen(4000, () => {
     console.log("Server listening on port 4000")
